4d61e1c8fe84d6efdd139f0591aff6b45117a41a
v1v <v1v@v1vs-MacBook-Air.local>
2025-06-11 16:06:12 +0530

    Update app to bind to Render's port

--- app.py
+++ app.py
@@ -25,4 +25,7 @@ def calculate():
     return jsonify({'sum': total})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
